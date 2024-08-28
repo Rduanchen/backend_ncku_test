40cca2fdef87178eeb8cd254e9ccc8e83ed6c407
hank93513 <hank93513@gmail.com>
2024-08-28 14:11:31 +0800

    feat: bad-smell modular programming

--- main.py
+++ main.py
@@ -2,16 +2,9 @@ import json
 import sentry_sdk
 from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi.middleware.cors import CORSMiddleware
-from .crawler.base import NewsWithSummary
 import itertools
 from sqlalchemy import delete, insert, select
 from sqlalchemy.orm import Session, sessionmaker
-from src.models import NewsArticle, User, engine, user_news_association_table
-from src.schemas import (NewsArticleSchema, NewsSumaryRequestSchema,
-                 NewsSummarySchema, PromptRequest,
-                 SearchNewsArticleSchema, NecessityPrice, TokenSchema, UserAuthSchema, UserSchema)
-from src.crawler import UDNCrawler
-from src.services.openai_client import openai_client
 from typing import List, Optional
 import requests
 from fastapi import APIRouter, HTTPException, Query, Depends, status, FastAPI
@@ -21,6 +14,159 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
+from pydantic import AnyHttpUrl
+
+from pydantic import BaseModel, Field, AnyHttpUrl
+from sqlalchemy import (Column, ForeignKey, Integer, String, Table, Text,
+                        create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
+
+user_news_association_table = Table(
+    "user_news_upvotes",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id"), primary_key=True),
+    Column(
+        "news_articles_id", Integer, ForeignKey("news_articles.id"), primary_key=True
+    ),
+)
+
+from pydantic import BaseModel
+
+
+class NecessityPrice(BaseModel):
+    類別: str
+    編號: int
+    產品名稱: str
+    規格: str
+    統計值: str
+    時間起點: str
+    時間終點: str
+
+
+class NewsArticleSchema(BaseModel):
+    id: int
+    url: str
+    title: str
+    time: str
+    content: str
+    summary: str
+    reason: str
+    upvotes: int
+    is_upvoted: bool
+
+    class Config:
+        from_attributes = True
+
+
+class SearchNewsArticleSchema(BaseModel):
+    id: int
+    url: str
+    title: str
+    time: str
+    content: str
+
+
+class NewsSummarySchema(BaseModel):
+    summary: str
+    reason: str
+
+
+class NewsSumaryRequestSchema(BaseModel):
+    content: str
+
+
+class PromptRequest(BaseModel):
+    prompt: str
+
+
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class UserSchema(BaseModel):
+    username: str
+
+    class Config:
+        from_attributes = True
+
+
+class UserAuthSchema(BaseModel):
+    username: str
+    password: str
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    hashed_password = Column(String(200), nullable=False)
+    upvoted_news = relationship(
+        "NewsArticle",
+        secondary=user_news_association_table,
+        back_populates="upvoted_by_users",
+    )
+
+
+class NewsArticle(Base):
+    __tablename__ = "news_articles"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String, unique=True, nullable=False)
+    title = Column(String, nullable=False)
+    time = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    summary = Column(Text, nullable=False)
+    reason = Column(Text, nullable=False)
+    upvoted_by_users = relationship(
+        "User", secondary=user_news_association_table, back_populates="upvoted_news"
+    )
+
+
+engine = create_engine("sqlite:///news_database.db", echo=True)
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+
+class Headline(BaseModel):
+    title: str = Field(
+        default=...,
+        example="Title of the article",
+        description="The title of the article"
+    )
+    url: AnyHttpUrl | str = Field(
+        default=...,
+        example="https://www.example.com",
+        description="The URL of the article"
+    )
+
+class News(Headline):
+    time: str = Field(
+        default=...,
+        example="2021-10-01T00:00:00",
+        description="The time the article was published"
+    )
+    content: str = Field(
+        default=...,
+        example="Content of the article",
+        description="The content of the article"
+    )
+
+class NewsWithSummary(News):
+    summary: str = Field(
+        default=...,
+        example="Summary of the article",
+        description="The summary of the article"
+    )
+    reason: str = Field(
+        default=...,
+        example="Reason of the article",
+        description="The reason of the article"
+    )
 
 sentry_sdk.init(
     dsn="https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000",
@@ -44,6 +190,173 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY", "")
+
+
+class OpenAIClient:
+    def __init__(self, _api_key: str):
+        self.client = OpenAI(api_key=_api_key)
+
+    def _generate_text(self, messages=None, model="gpt-3.5-turbo"):
+        if not messages:
+            messages = [
+                {
+                    "role": "system",
+                    "content": "你是一個對話機器人，請回答以下問題。",
+                }
+            ]
+        completion = self.client.chat.completions.create(
+            model=model,
+            messages=messages,
+        )
+        return completion.choices[0].message.content
+
+    def evaluate_relevance(self, title):
+        messages = [
+            {
+                "role": "system",
+                "content": "你是一個關聯度評估機器人，請評估新聞標題是否與「民生用品的價格變化」相關，並給予'high'、'medium'、'low'評價。(僅需回答'high'、'medium'、'low'三個詞之一)",
+            },
+            {"role": "user", "content": f"{title}"},
+        ]
+        return openai_client._generate_text(messages=messages)
+
+    def generate_summary(self, content):
+        messages = [
+            {
+                "role": "system",
+                "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
+            },
+            {"role": "user", "content": f"{content}"},
+        ]
+        return openai_client._generate_text(messages=messages)
+
+    def extract_search_keywords(self, content):
+        messages = [
+            {
+                "role": "system",
+                "content": "你是一個關鍵字提取機器人，用戶將會輸入一段文字，表示其希望看見的新聞內容，請提取出用戶希望看見的關鍵字，請截取最重要的關鍵字即可，避免出現「新聞」、「資訊」等混淆搜尋引擎的字詞。(僅須回答關鍵字，若有多個關鍵字，請以空格分隔)",
+            },
+            {"role": "user", "content": f"{content}"},
+        ]
+        return openai_client._generate_text(messages=messages)
+
+
+openai_client = OpenAIClient(_api_key=api_key)
+
+
+from urllib.parse import quote
+import requests
+from requests import Response
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import Session
+
+
+class UDNCrawler():
+    CHANNEL_ID = 2
+
+    def __init__(self, timeout = 5):
+        self.news_website_url = "https://udn.com/api/more"
+        self.timeout = timeout
+
+    def startup(self, search_term: str) -> list[Headline]:
+        headlines = []
+        for p in range(1, 10):
+            params = {
+                "page": p,
+                "id": f"search:{quote(search_term)}",
+                "channelId": self.CHANNEL_ID,
+                "type": "searchword",
+            }
+            response = self.perform_request(params=params)
+            headline_list = self.parse_headlines(response) if response else []
+            for headline in headline_list:
+                headlines.append(headline)
+        return headlines
+
+    def get_headline(self, search_term, page):
+        headlines = []
+        page_range = range(*page) if isinstance(page, tuple) else [page]
+        for p in page_range:
+            params = {
+                "page": p,
+                "id": f"search:{quote(search_term)}",
+                "channelId": self.CHANNEL_ID,
+                "type": "searchword",
+            }
+            response = self.perform_request(params=params)
+            headline_list = self.parse_headlines(response) if response else []
+            for headline in headline_list:
+                headlines.append(headline)
+        return headlines
+
+    def perform_request(self, url = None, params = None) -> Response:
+        if not url:
+            url = self.news_website_url
+
+        if not params:
+            params = {}
+
+        try:
+            response = requests.get(
+                url, params=params, timeout=self.timeout
+            )
+            response.raise_for_status()
+            return response
+        except requests.RequestException as e:
+            raise ConnectionError(f"Error fetching news: {e}") from e
+
+    @staticmethod
+    def parse_headlines(response: Response):
+        data = response.json().get("lists", [])
+        return [
+            Headline(title=article["title"], url=article["titleLink"])
+            for article in data
+        ]
+
+    def parse(self, url):
+        response = self.perform_request(url=url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        return self.extract_news(soup, url)
+
+    @staticmethod
+    def extract_news(soup: BeautifulSoup, url):
+        title = soup.select_one("h1.article-content__title").text
+        time = soup.select_one("time.article-content__time").text
+        content = " ".join(
+            p.text
+            for p in soup.select("section.article-content__editor p")
+            if p.text.strip()
+        )
+        return News(url=url, title=title, time=time, content=content, reason="", summary="")
+
+    def save(self, news: NewsWithSummary, db: Session):
+        existing_news = db.query(NewsArticle).filter_by(url=news.url).first()
+        if not existing_news:
+            new_article = NewsArticle(
+                url=news.url, title=news.title, time=news.time, content=news.content, summary=news.summary, reason=news.reason
+            )
+            db.add(new_article)
+            self.commit_changes(db)
+
+    @staticmethod
+    def commit_changes(db: Session):
+        try:
+            db.commit()
+        except Exception as e:
+            db.rollback()
+            raise RuntimeError(f"Error saving news to database: {e}") from e
+        finally:
+            db.close()
+
+
 
 def fetch_news_task(is_init: bool = False):
     db = SessionLocal()
@@ -101,7 +414,7 @@ async def trigger_error():
 
 
 # users api
-users_router = APIRouter()
+app = APIRouter()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
@@ -166,7 +479,7 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     return encoded_jwt
 
 
-@users_router.post("/login", response_model=TokenSchema)
+@app.post("/login", response_model=TokenSchema)
 async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(session_opener)
 ):
@@ -184,7 +497,7 @@ async def login_for_access_token(
     return {"access_token": access_token, "token_type": "bearer"}
 
 
-@users_router.post("/register", response_model=UserSchema)
+@app.post("/register", response_model=UserSchema)
 def create_user(user: UserAuthSchema, db: Session = Depends(session_opener)):
     db_user = db.query(User).filter(User.username == user.username).first()
     if db_user:
@@ -197,13 +510,13 @@ def create_user(user: UserAuthSchema, db: Session = Depends(session_opener)):
     return db_user
 
 
-@users_router.get("/me", response_model=UserSchema)
+@app.get("/me", response_model=UserSchema)
 def read_users_me(user: str = Depends(authenticate_user_token)):
     return {"username": user.username}
 
 
 #news api
-news_router = APIRouter()
+app = APIRouter()
 
 udn_crawler = UDNCrawler()
 
@@ -226,7 +539,7 @@ def get_article_upvote_details(article_id, uid, db):
     return cnt, voted
 
 
-@news_router.get("/news", response_model=list[NewsArticleSchema])
+@app.get("/news", response_model=list[NewsArticleSchema])
 def read_news(db: Session = Depends(session_opener)):
     news = db.query(NewsArticle).order_by(NewsArticle.time.desc()).all()
     result = []
@@ -238,7 +551,7 @@ def read_news(db: Session = Depends(session_opener)):
     return result
 
 
-@news_router.get(
+@app.get(
     "/user_news",
     response_model=list[NewsArticleSchema],
     description="獲取包含user upvote資訊的新聞列表",
@@ -260,7 +573,7 @@ def read_user_news(
     return result
 
 
-@news_router.post("/search_news", response_model=list[SearchNewsArticleSchema])
+@app.post("/search_news", response_model=list[SearchNewsArticleSchema])
 async def search_news(request: PromptRequest):
     prompt = request.prompt
     newss = []
@@ -281,7 +594,7 @@ async def search_news(request: PromptRequest):
     return sorted(newss, key=lambda x: x["time"], reverse=True)
 
 
-@news_router.post("/news_summary", response_model=NewsSummarySchema)
+@app.post("/news_summary", response_model=NewsSummarySchema)
 async def news_summary(
     payload: NewsSumaryRequestSchema, user=Depends(authenticate_user_token)
 ):
@@ -294,7 +607,7 @@ async def news_summary(
         response["reason"] = result["原因"]
     return response
 
-@news_router.post("/{id}/upvote")
+@app.post("/{id}/upvote")
 def upvote_article(
     id: int,
     db: Session = Depends(session_opener),
@@ -339,9 +652,9 @@ def exists(news_id: int, db: Session):
     return db.query(NewsArticle).filter_by(id=news_id).first() is not None
 
 
-prices_router = APIRouter()
+app = APIRouter()
 
-@prices_router.get("/necessities-price", response_model=List[NecessityPrice])
+@app.get("/necessities-price", response_model=List[NecessityPrice])
 def get_necessities_prices(
     category: Optional[str] = Query(None), commodity: Optional[str] = Query(None)
 ):
@@ -354,9 +667,4 @@ def get_necessities_prices(
 
     return response.json()
 
-api_router = APIRouter()
-api_router.include_router(prices_router, prefix="/prices", tags=["prices"])
-api_router.include_router(news_router, prefix="/news", tags=["news"])
-api_router.include_router(users_router, prefix="/users", tags=["users"])
-
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(app, prefix="/v1")
